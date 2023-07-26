9ed9b77c2df995fb8740eee80879c72e2dfda61c
Kanhamolax <gui.bianchini@gmail.com>
2023-07-25 21:40:13 -0400

    bancoooo

---
+++ banco/Banco/ConsoleApp1/Conta.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    internal class Conta
+    {
+        public double saldo{ get;  private set; }
+    public string nome { get;  set; }
+    public int numereoConta { get; private set; }
+        public Conta(double saldo, string nome,int numeroConta)
+        {
+            this.saldo = saldo;
+            this.nome = nome;
+            this.numereoConta = numeroConta;
+        }
+        public Conta()
+        {
+            this.saldo = 0;
+            this.nome = "";
+            this.numereoConta = 0;
+        }
+        public void CriaConta(string nome,double valor)
+        {
+           
+            this.nome = nome;
+            this.numereoConta=123321;
+            this.saldo = valor;
+
+        }
+        public void CriaConta(string nome)
+        {
+
+            this.nome = nome;
+            this.numereoConta = 123321;
+            this.saldo = 0;
+
+        }
+       
+        public void MostraSaldo()
+        {
+
+            Console.WriteLine(this.nome+" Seu saldo é:");
+            Console.WriteLine(this.saldo);
+
+        }
+      public  void Deposita(double valor)
+        {
+           
+            
+          this.saldo += valor;
+
+        }
+        public void Saque(double valor )
+        {
+          
+            this.saldo -= valor+5;
+
+        }
+        public void Atualiza(string nome)
+        {
+           
+           
+            this.nome = nome;
+           
+
+        }
+
+    }
+}
---
+++ banco/Banco/ConsoleApp1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+namespace Banco // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int op;
+            double valor;
+            Conta conta=new Conta();
+            do
+            {
+                Console.WriteLine("*********Banco************");
+                Console.WriteLine("1-Criar Conta");
+                Console.WriteLine("2-Consultar Saldo");
+                Console.WriteLine("3-Deposito");
+                Console.WriteLine("4-Saque");
+                Console.WriteLine("5-Atualizar dados");
+
+                Console.WriteLine("Digite uma opção :");
+                op = int.Parse(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 1:
+                        string nome;
+                        int op2;
+                        double saldo;
+                        Console.WriteLine("Digite seu nome");
+                        nome = Console.ReadLine();
+
+                        Console.WriteLine("Ira adicionar saldio inicial ?");
+                        Console.WriteLine("1-Sim");
+                        Console.WriteLine("2-Não");
+                        op2 = int.Parse(Console.ReadLine());
+
+                        switch (op2)
+                        {
+                            case 1:
+                                Console.WriteLine("Digite o valor do saldo inicial :");
+                                saldo = double.Parse(Console.ReadLine());
+                                conta.CriaConta(nome, saldo);
+
+
+                                break;
+                            case 2:
+
+                                conta.CriaConta(nome);
+                                break;
+
+                        }
+                       
+                        break;
+                    case 2:
+                        conta.MostraSaldo();
+                        break;
+                  
+                    case 3:
+                        Console.WriteLine("Digite o valo que quer depositar:");
+                        valor = double.Parse(Console.ReadLine());
+                        conta.Deposita(valor );
+                        break;
+                    case 4:
+
+                        Console.WriteLine("Digite o valo que quer sacar:");
+                        valor = double.Parse(Console.ReadLine());
+                        conta.Saque(valor);
+                        break;
+                    case 5:
+                        Console.WriteLine("Digite o nome:");
+                        nome = Console.ReadLine();
+                        conta.Atualiza(nome );
+                        break;
+                }
+
+
+            } while (true);
+
+           
+        }
+       
+       
+
+
+
+    }
+}
